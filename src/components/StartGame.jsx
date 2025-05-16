d8f4cae36990cc74d78c92c74ba0e80ebabc8466
Flex58 <164533598+flex58@users.noreply.github.com>
2025-05-16 11:14:13 +0200

    style: change wording in StartGame

--- src/components/StartGame.jsx
+++ src/components/StartGame.jsx
@@ -4,10 +4,13 @@ export default function StartGame({ onclick }) {
       <h1>Pokemon Memory Game</h1>
       <h2>Goal: </h2>
       <p>
-        Click as many unique Pokemon in a row! When you click one you have
-        already clicked, you lose! Try to increase your High Score!!
+        Click as many unique Pokemon in a row as you can! When you click a
+        Pokemon you have already clicked, you lose! Try to increase your High
+        Score!!
       </p>
-      <button type="button" onclick={onclick}>Start Game</button>
+      <button type="button" onclick={onclick}>
+        Start Game
+      </button>
     </>
   );
 }
